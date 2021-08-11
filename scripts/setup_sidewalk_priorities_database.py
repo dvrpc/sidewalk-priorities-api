bd163d7e2049f19e422013bbd60d73ed25fc1b1d
aaronfraint <aaronfraint@icloud.com>
2021-08-11 13:27:37 -0400

    plug into new helper function

--- scripts/setup_sidewalk_priorities_database.py
+++ scripts/setup_sidewalk_priorities_database.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 
+from .helpers import copy_local_tables_to_cloud
+
 load_dotenv(find_dotenv())
 
-DB_URLS = {
+DB_URIS = {
     "cloud": os.environ.get("SUPERUSER_SIDEWALK_PRIORITIES_DATABASE_URL"),
     "local": os.environ.get("LOCAL_SIDEWALK_PRIORITIES_DATABASE_URL"),
 }
@@ -12,16 +14,10 @@ TABLES_TO_COPY = ["api.missing_links", "api.isochrones"]
 
 def main() -> None:
     """
-    - For each table to copy, run a terminal command to pipe data from local DB to cloud DB
-    - Command uses:
-        > pg_dump LOCAL_URI -t TABLENAME --no-owner | psql CLOUD_URI
+    - Copy the necessary sidewalk and ischrone tables
     """
 
-    for table in TABLES_TO_COPY:
-        cmd = f"pg_dump {DB_URLS['local']} -t {table} --no-owner | psql {DB_URLS['cloud']}"
-        print(cmd)
-
-        os.system(cmd)
+    copy_local_tables_to_cloud(TABLES_TO_COPY, DB_URIS["local"], DB_URIS["cloud"])
 
 
 if __name__ == "__main__":
