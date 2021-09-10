a7bb8da9617e5eb2e0c226f4e2d888c7df929d21
aaronfraint <aaronfraint@icloud.com>
2021-09-10 14:23:44 -0400

    load additional spatial tables to cloud db

--- scripts/setup_sidewalk_priorities_database.py
+++ scripts/setup_sidewalk_priorities_database.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 
-from .helpers import copy_local_tables_to_cloud
 
 load_dotenv(find_dotenv())
 
@@ -9,15 +8,24 @@ DB_URIS = {
     "cloud": os.environ.get("SUPERUSER_SIDEWALK_PRIORITIES_DATABASE_URL"),
     "local": os.environ.get("LOCAL_SIDEWALK_PRIORITIES_DATABASE_URL"),
 }
-TABLES_TO_COPY = ["api.missing_links", "api.isochrones"]
+TABLES_TO_COPY = [
+    # "api.missing_links",
+    # "api.isochrones",
+    # "api.montco_munis",
+    "api.pois",
+]
 
 
 def main() -> None:
     """
-    - Copy the necessary sidewalk and ischrone tables
+    - Copy the necessary sidewalk, isochrone, and municipality tables
     """
 
-    copy_local_tables_to_cloud(TABLES_TO_COPY, DB_URIS["local"], DB_URIS["cloud"])
+    for table in TABLES_TO_COPY:
+        cmd = f"pg_dump {DB_URIS['local']} -t {table} --no-owner | psql {DB_URIS['cloud']}"
+
+        print(cmd)
+        os.system(cmd)
 
 
 if __name__ == "__main__":
