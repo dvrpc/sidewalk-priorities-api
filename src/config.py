a1b25c2cae2a9f25847ec6f1fbcfa3963b81be5a
Kris Warner <kdwarn@protonmail.com>
2021-09-29 17:12:38 -0400

    Flatten project
    Remove the /sidewalk prefix and incorporate those endpoints directly
    into the main file, move db.py into src dir. Only pin the dependencies
    directly used, not the dependencies of dependences. Add a default
    URL_ROOT.

--- src/config.py
+++ src/config.py
@@ -5,4 +5,4 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-URL_ROOT = os.environ.get("URL_ROOT")
+URL_ROOT = os.environ.get("URL_ROOT", "")
